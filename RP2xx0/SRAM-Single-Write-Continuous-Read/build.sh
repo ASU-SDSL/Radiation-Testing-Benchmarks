583e720d1ada9f0aa91eb44995aa9413d5ee8053
SnailDragon <tylernielsenn@gmail.com>
2025-11-18 13:50:20 -0700

    start of sram benchmarks

---
+++ RP2xx0/SRAM-Single-Write-Continuous-Read/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clang-format -i --style=Google main.cpp
+
+mkdir -p build
+cd build; 
+cmake -DPICO_SDK_PATH=$(pwd)/../../pico-sdk ..;
+make;
+
+mkdir -p ../_uf2/
+cp radio_test.uf2 ../_uf2/

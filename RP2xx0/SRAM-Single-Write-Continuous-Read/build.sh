6ab32459ac4696e88c4fea8f6673c9aabf7f8858
SnailDragon <tylernielsenn@gmail.com>
2025-11-18 15:34:21 -0700

    rp2040 builds rp2350 doesn't

--- RP2xx0/SRAM-Single-Write-Continuous-Read/build.sh
+++ RP2xx0/SRAM-Single-Write-Continuous-Read/build.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
 
-clang-format -i --style=Google main.cpp
+RP2040_SRAM_TO_USE_B=$((200*1000))
+RP2350_SRAM_TO_USE_B=$((400*1000)) 
 
-mkdir -p build
-cd build; 
-cmake -DPICO_SDK_PATH=$(pwd)/../../pico-sdk ..;
+clang-format -i --style=Google *.cpp
+echo "Formatted source files"
+
+# # build for RP2040
+# mkdir -p build_2040
+# cd build_2040; 
+# cmake -DPICO_SDK_PATH=$(pwd)/../../pico-sdk \
+#       -DCMAKE_CXX_FLAGS="-DSRAM_TO_USE_B=$RP2040_SRAM_TO_USE_B" \
+#       -DPICO_PLATFORM=rp2040 \
+#       -DPICO_BOARD=pico \
+#       ..;
+# make;
+
+# mkdir -p ../_uf2/
+# cp *.uf2 ../_uf2/sram_single_write_continuous_read_rp2040.uf2
+
+# echo "Built for RP2040"
+# cd ..;
+
+# build for RP2350 (ARM)
+mkdir -p build_2350_arm
+cd build_2350_arm; 
+cmake -DPICO_SDK_PATH=$(pwd)/../../pico-sdk \
+      -DCMAKE_CXX_FLAGS="-DSRAM_TO_USE_B=$RP2040_SRAM_TO_USE_B" \
+      -DPICO_PLATFORM=rp2350 \
+      -DPICO_BOARD=pico2 \
+      ..;
 make;
 
-mkdir -p ../_uf2/
-cp radio_test.uf2 ../_uf2/
+cp *.uf2 ../_uf2/sram_single_write_continuous_read_rp2350_arm.uf2
+echo "Built for RP2350 (ARM cores)"
+cd ..;
+
+# # build for RP2350 (RISC-V)
+# mkdir -p build_2350_riscv
+# cd build_2350_riscv; 
+# cmake -DPICO_SDK_PATH=$(pwd)/../../pico-sdk \
+#       -DCMAKE_CXX_FLAGS="-DSRAM_TO_USE_B=$RP2040_SRAM_TO_USE_B" \
+#       -DPICO_PLATFORM=rp2350-riscv \
+#       -DPICO_BOARD=pico2 \
+#       ..;
+# make;
+
+# cp *.uf2 ../_uf2/sram_single_write_continuous_read_rp2350_riscv.uf2
+# echo "Built for RP2350 (RISC-V cores)"
+# cd ..;
--- RP2xx0/SRAM-Single-Write-Continuous-Read/main.cpp
+++ RP2xx0/SRAM-Single-Write-Continuous-Read/main.cpp
@@ -1,6 +1,45 @@
+#include <stdint.h>
+#include <stdio.h>
 
+#include <cstdlib>
 
-int main(){
+#define PICO_MALLOC_PANIC 0
 
-  return 0; 
+#include "pico/stdlib.h"
+
+// if not specified use ~200kB, should be pretty safe
+#ifndef SRAM_TO_USE_B
+#define SRAM_TO_USE_B 200 * 1000  // ~200kB can probably be higher
+#endif
+
+// pattern defined as so to have it reread from flash each time
+#define PATTERN \
+  "\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF"
+
+int main() {
+  stdio_init_all();
+
+  // create memory
+  // on the RP2xx0 the SRAM is striped across blocks, but really we don't care
+  // about that because we are reading it as a logical block so can just use an
+  // arbitrarily large dynamic memory allocation with a volatile designation
+  volatile uint8_t* data =
+      (volatile uint8_t*)malloc(SRAM_TO_USE_B * sizeof(uint8_t));
+
+  // write pattern
+  for (size_t i = 0; i < SRAM_TO_USE_B; i++) {
+    data[i] = PATTERN[i % sizeof(PATTERN)];
+  }
+
+  // read and report every 1s
+  while (1) {
+    uint32_t timestamp = to_ms_since_boot(get_absolute_time());
+    printf("Reading at %lu:\n", timestamp);
+    for (size_t i = 0; i < SRAM_TO_USE_B; i++) {
+      printf("%x", data[i]);
+    }
+    printf("\nDone (started: %lu).", timestamp);
+  }
+
+  return 0;
 }

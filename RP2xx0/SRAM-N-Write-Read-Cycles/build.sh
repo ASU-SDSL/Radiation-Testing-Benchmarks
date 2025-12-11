e5b116368a6f127787e01b1bf5b246cbf0e82d6e
SnailDragon <tylernielsenn@gmail.com>
2025-12-10 22:30:15 -0700

    initial build for sram-n-write-read-cycles and better .gitignore structure

---
+++ RP2xx0/SRAM-N-Write-Read-Cycles/build.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+function print_separator {
+      WIDTH=$(tput cols)
+      CHAR='-'
+      LINE=$(printf '%*s' "${WIDTH}" '' | tr ' ' "${CHAR}")
+      echo -e "\n$LINE\n"
+}
+
+function args_contain() {
+      local target="$1"
+
+      shift # discard local arg 
+
+      if [[ "$#" -eq 0 || "$*" =~ "all" || "$*" =~ "$target" ]]; then 
+            return 0
+      else 
+            return 1
+      fi
+}
+
+RP2040_SRAM_TO_USE_B=$((200*1000))
+RP2350_SRAM_TO_USE_B=$((400*1000)) 
+
+if [[ -n "$1" && "$1" == "--help" ]]; then
+    echo "Usage: build.sh [all|rp2040|rp2350-arm|rp2350-riscv]"
+    echo "If no arguments are provided, builds for all platforms."
+    exit 0
+fi
+
+clang-format -i --style=Google *.cpp
+echo "Formatted source files"
+
+# set up PICO_SDK_PATH and save old version 
+OLD_PICO_SDK_PATH="$PICO_SDK_PATH"
+
+export PICO_SDK_PATH="$(pwd)/../pico-sdk"
+
+# determine what to build based on args
+if args_contain "rp2040" "$@"; then 
+      # build for RP2040
+      mkdir -p build_2040
+
+      cmake -S . -B build_2040 \
+            -DPICO_PLATFORM=rp2040 \
+            -DPICO_BOARD=pico \
+            -DSRAM_TO_USE_B=$RP2040_SRAM_TO_USE_B \
+            ;
+      cmake --build build_2040 -- -j$(nproc);
+
+      mkdir -p _uf2
+      cp build_2040/*.uf2 _uf2/sram_single_write_continuous_read_rp2040.uf2
+      echo -e "Built for RP2040"
+
+      print_separator
+fi
+
+if args_contain "rp2350-arm" "$@"; then 
+      # build for RP2350 (ARM)
+      mkdir -p build_2350_arm
+
+      cmake -S . -B build_2350_arm \
+            -DPICO_PLATFORM=rp2350-arm-s \
+            -DPICO_BOARD=pico2 \
+            -DSRAM_TO_USE_B=$RP2350_SRAM_TO_USE_B \
+            ;
+      cmake --build build_2350_arm -- -j$(nproc);
+
+      mkdir -p _uf2
+      cp build_2350_arm/*.uf2 _uf2/sram_single_write_continuous_read_rp2350_arm.uf2
+      echo "Built for RP2350 (ARM cores)"
+
+      print_separator
+fi
+
+if args_contain "rp2350-riscv" "$@"; then 
+      # build for RP2350 (RISC-V)
+      mkdir -p build_2350_riscv
+
+      cmake -S . -B build_2350_riscv \
+            -DPICO_TOOLCHAIN_PATH=$(pwd)/../_tools/riscv/riscv-toolchain-15 \
+            -DPICO_PLATFORM=rp2350-riscv \
+            -DPICO_BOARD=pico2 \
+            -DSRAM_TO_USE_B=$RP2350_SRAM_TO_USE_B \
+            ;
+      cmake --build build_2350_riscv -- -j$(nproc);
+
+      mkdir -p _uf2
+      cp build_2350_riscv/*.uf2 _uf2/sram_single_write_continuous_read_rp2350_riscv.uf2
+      echo "Built for RP2350 (RISC-V cores)"
+
+      print_separator
+fi
+
+# restore old PICO_SDK_PATH
+if [ -n "$OLD_PICO_SDK_PATH" ]; then
+    export PICO_SDK_PATH="$OLD_PICO_SDK_PATH"
+else
+    unset PICO_SDK_PATH
+fi
+
+# print final message and info 
+echo "Finished running build scripts for: "
+if args_contain "rp2040" "$@"; then 
+    echo "- RP2040"
+fi
+if args_contain "rp2350-arm" "$@"; then 
+    echo "- RP2350 (ARM cores)"
+fi
+if args_contain "rp2350-riscv" "$@"; then 
+    echo "- RP2350 (RISC-V cores)"
+fi
---
+++ RP2xx0/SRAM-N-Write-Read-Cycles/main.cpp
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include <cstdlib>
+
+#include "tusb.h"
+
+#define PICO_MALLOC_PANIC 0
+
+#include "pico/stdlib.h"
+
+// if not specified use ~200kB, should be pretty safe
+#ifndef SRAM_TO_USE_B
+#define SRAM_TO_USE_B 200 * 1000  // ~200kB can probably be higher
+#endif
+
+// pattern defined as so to have it reread from flash each time
+#define PATTERN "1234567890abcdefghijklmnopqrstuv"
+#define CHUNK_SIZE 64  // max for the pico sdk usb buffer
+
+int main() {
+  stdio_init_all();
+
+  sleep_ms(10000);  // wait 10s
+
+  printf("Starting Test\n\n");
+
+  // create memory
+  // on the RP2xx0 the SRAM is striped across blocks, but really we don't care
+  // about that because we are reading it as a logical block so can just use an
+  // arbitrarily large dynamic memory allocation with a volatile designation
+  volatile uint8_t* data =
+      (volatile uint8_t*)malloc(SRAM_TO_USE_B * sizeof(uint8_t));
+
+  // read and report
+  while (1) {
+    uint32_t timestamp = to_ms_since_boot(get_absolute_time());
+    printf("\n[INFO] Writing pattern to SRAM at %lu:\n", timestamp);
+    // write pattern
+    for (size_t i = 0; i < SRAM_TO_USE_B; i++) {
+      data[i] = PATTERN[i % (sizeof(PATTERN) - 1)];  // -1 to avoid the \0
+    }
+    printf("Data written (started at %d)\n", timestamp);
+
+    timestamp = to_ms_since_boot(get_absolute_time());
+    printf("\n[INFO] Reading at %lu:\n", timestamp);
+    // transferring at max CHUNK_SIZE for pico's buffer for better speed
+    for (size_t i = 0; i < SRAM_TO_USE_B; i += CHUNK_SIZE) {
+      tud_cdc_write((void*)&(data[i]), CHUNK_SIZE);
+      tud_cdc_write_flush();
+    }
+    printf("\n[INFO] Done (started: %lu).\n", timestamp);
+    sleep_ms(5000);  // wait 5s between reads
+  }
+
+  return 0;
+}

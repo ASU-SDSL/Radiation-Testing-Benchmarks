3b5fae78f809fab2d7410cc82ddc5477337c1d5e
SnailDragon <tylernielsenn@gmail.com>
2025-12-12 13:22:54 -0700

    passive-no-operation back on flash and turns on led

--- RP2xx0/Passive-No-Operation/main.cpp
+++ RP2xx0/Passive-No-Operation/main.cpp
@@ -2,8 +2,15 @@
  * Do nothing for Passive Radiation Test
  *
  */
+#include <pico/stdlib.h>
+
+#define BUILT_IN_LED_PIN 25
 
 int main() {
+  gpio_init(BUILT_IN_LED_PIN);
+  gpio_set_dir(BUILT_IN_LED_PIN, GPIO_OUT);
+  gpio_put(BUILT_IN_LED_PIN, 1);  // turn on built-in
+
   while (1);  // do nothing
 
   return 0;

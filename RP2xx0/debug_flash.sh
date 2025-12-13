c434461c1ef63b3d9aae454a1d748502f6a8aa53
SnailDragon <tylernielsenn@gmail.com>
2025-12-13 00:33:21 -0700

    fixed paths in debug_flash.sh

--- RP2xx0/debug_flash.sh
+++ RP2xx0/debug_flash.sh
@@ -10,9 +10,9 @@ fi
 echo "Flashing target device $1..."
 
 if [[ "$1" = "rp2040" ]]; then 
-    sudo openocd -f interface/cmsis-dap.cfg -f ../debug/rp2040.cfg -c "adapter speed 5000" -c "program $2 verify reset exit"
+    sudo openocd -f interface/cmsis-dap.cfg -f debug/rp2040.cfg -c "adapter speed 5000" -c "program $2 verify reset exit"
 elif [[ "$1" = "rp2350" ]]; then
-    sudo openocd -f interface/cmsis-dap.cfg -f ../debug/rp2350.cfg -c "adapter speed 5000" -c "program $2 verify reset exit"
+    sudo openocd -f interface/cmsis-dap.cfg -f debug/rp2350.cfg -c "adapter speed 5000" -c "program $2 verify reset exit"
 else 
     echo "Unsupported target device: $1"
     exit 1

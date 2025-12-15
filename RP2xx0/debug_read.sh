eb526a9cdbeb91e011e2f66f07a95ef3f748ef5e
SnailDragon <tylernielsenn@gmail.com>
2025-12-14 18:03:36 -0700

    added direct register read

--- RP2xx0/debug_read.sh
+++ RP2xx0/debug_read.sh
@@ -9,14 +9,29 @@ fi
 
 echo "Flashing target device $1..."
 
+# read registers in order R0-R12, SP, LR, PC
+
 if [[ "$1" = "rp2040" ]]; then 
     sudo openocd -f interface/cmsis-dap.cfg -f debug/rp2040.cfg \
         -c "adapter speed 5000" \
         -c "init" -c "targets rp2040.core0" -c "halt" \
-        -c "mdw 0xE000ED00 1" \
-        -c "mdw 0xE000EDF0 1" \
-        -c "mdw 0x20000000 1" \
-        -c "reg" \
+        -c "log_output regs.txt" \
+        -c "mww 0xE000EDF4 0x10000" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10001" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10002" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10003" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10004" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10005" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10006" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10007" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10008" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x10009" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000A" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000B" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000C" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000D" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000E" -c "mdw 0xE000EDF8" \
+        -c "mww 0xE000EDF4 0x1000F" -c "mdw 0xE000EDF8" \
         -c "log_output default" \
         -c "dump_image out.bin 0x20000000 0x42000" \
         -c "resume" -c "shutdown"
